bb9ec11284a0ebf0355e487fcd2ea3f9025f3914
Julie Greene <julie_greene22@yahoo.com>
2020-03-10 17:38:07 -0600

    cards designed and buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Container, Header, Grid, } from 'semantic-ui-react';
+import Flashcards from './Flashcards';
 
-function App() {
+
+class App extends React.Component {
+  state = {
+    flashcards: [
+      {id:1, front:"What is 10 * 2?", back:"20"},
+      {id:2, front:"What is 400 - 100?", back:"300"},
+      {id:3, front:"What is 75 + 25?", back:"100"},
+      {id:4, front:"What is 16 / 2?", back:"8"},
+      {id:5, front:"What is 4 ^ 2?", back:"16"},
+      {id:6, front:"What is 2 * 3 + 5?", back:"11"},
+    ]
+  };
+
+mapFlashcards = () => {
+  return(
+    this.state.flashcards.map(flashcard => (
+      <Flashcards key={flashcard.id} flashcard={{...flashcard}} />
+    ))
+  )
+}
+
+removeFlashcard = (id) => {
+  const flashcards = this.state.flashcards.filter( flashcard => {
+    if (flashcard.id !== id)
+      return flashcard 
+  });
+  this.setState({ flashcards: [...flashcards], })
+};
+
+
+
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div>
+      <Header textAlign= "center" as='h1'>Math Flashcards</Header>
+      <hr />
+      </div>
+        <Grid columns={3}>
+          <Grid.Row>
+            <Grid.Column>
+              {this.mapFlashcards()} 
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+    </Container>
+   
   );
+ }
 }
 
 export default App;
---
+++ src/Flashcards.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card, Button, Image } from 'semantic-ui-react';
+import image from './math.jpg'
+
+const Flashcards = ({ flashcard }) => (
+  <Card>
+    <Card.Content>
+    <Image
+          floated='right'
+          size='mini'
+          src={image}
+        />
+      <Card.Header textAlign="center">{flashcard.front}</Card.Header>
+    </Card.Content>
+    <Card.Content extra>
+      <div className= 'ui two buttons'>
+        <Button basic color= 'black'>See Answer</Button> 
+        <Button basic color='black'>Delete</Button>
+      </div>
+    </Card.Content>
+  </Card>
+);
+
+export default Flashcards;
